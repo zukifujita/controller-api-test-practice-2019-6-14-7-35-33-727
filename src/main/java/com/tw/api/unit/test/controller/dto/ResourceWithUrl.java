301b74d29f18e06d249cf0410d73d8f167710136
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-16 21:36:06 +0800

    Commented @XmlAnyElement

--- src/main/java/com/tw/api/unit/test/controller/dto/ResourceWithUrl.java
+++ src/main/java/com/tw/api/unit/test/controller/dto/ResourceWithUrl.java
@@ -2,7 +2,7 @@ package com.tw.api.unit.test.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
-import javax.xml.bind.annotation.XmlAnyElement;
+//  import javax.xml.bind.annotation.XmlAnyElement;
 
 public class ResourceWithUrl<T> {
     private T content;
@@ -18,7 +18,7 @@ public class ResourceWithUrl<T> {
     }
 
     @JsonUnwrapped
-    @XmlAnyElement
+//    @XmlAnyElement
     public T getContent() {
         return content;
     }
